8d8395821a2b797e1eb30acea63baa720095e841
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-07-14 22:07:19 +0530

    Update PrimsAlgo.py

--- PrimsAlgo.py
+++ PrimsAlgo.py
@@ -1,67 +1,29 @@
-import sys
- 
- 
-class Graph():
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
- 
-    
-    def printMST(self, parent):
-        print("Edge \tWeight")
-        for i in range(1, self.V):
-            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
- 
-    
-    def minKey(self, key, mstSet):
- 
-        min = sys.maxsize
- 
-        for v in range(self.V):
-            if key[v] < min and mstSet[v] == False:
-                min = key[v]
-                min_index = v
- 
-        return min_index
- 
-    
-    def primMST(self):
- 
-        key = [sys.maxsize] * self.V
-        parent = [None] * self.V  
-        key[0] = 0
-        mstSet = [False] * self.V
- 
-        parent[0] = -1  
- 
-        for cout in range(self.V):
- 
-            
-            u = self.minKey(key, mstSet)
- 
-            
-            mstSet[u] = True
- 
-            
-            for v in range(self.V):
- 
-                
-                if self.graph[u][v] > 0 and mstSet[v] == False \
-                and key[v] > self.graph[u][v]:
-                    key[v] = self.graph[u][v]
-                    parent[v] = u
- 
-        self.printMST(parent)
- 
- 
-# Driver's code
-if __name__ == '__main__':
-    g = Graph(5)
-    g.graph = [[0, 2, 0, 6, 0],
-               [2, 0, 3, 8, 5],
-               [0, 3, 0, 0, 7],
-               [6, 8, 0, 0, 9],
-               [0, 5, 7, 9, 0]]
- 
-    g.primMST()
+from heapq import *
+
+def krushkal(graph):
+        visited = set()
+        source = 0
+        heap= [(0,source, source)]
+        ans = []
+        while heap:
+            w, node, parent = heappop(heap)
+            if node not in visited:
+                visited.add(node)
+                if node!=parent:
+                    ans.append((f"{parent}->{node}", w))
+                for child,wt in graph[node]:
+                    if child not in visited:
+                        heappush(heap,(wt,child,node))
+        ans.sort(key= lambda x: x[1])
+        for edge, weight in ans:
+            print(edge," : ",weight)
+        
+graph = {
+    0: [(1, 2), (3, 6)],
+    1: [(0, 2), (2, 3), (3, 8), (4, 5)],
+    2: [(1, 3), (4, 7)],
+    3: [(0, 6), (1, 8), (4, 9)],
+    4: [(1, 5), (2, 7), (3, 9)]
+}
+
+krushkal(graph)
